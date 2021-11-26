0195d854c976907a80ba0d820d61b8c4e7d10424
palchurl <66439919+palchurl@users.noreply.github.com>
2021-11-26 01:03:38 -0500

    added script to preprocess the tweets
    Applies emoji conversion, deletes URLs/mentions, removes digits, extra spaces, and punctuation, makes all text lowercase, removes most and least frequent words applies stemming and lemmatization.

---
+++ preprocessor.py
@@ -0,0 +1,101 @@
+import nltk
+import re, string
+import numpy as np
+import emoji
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
+from collections import Counter
+from nltk.stem.porter import PorterStemmer
+from nltk import word_tokenize, sent_tokenize
+import preprocessor as p
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+#tweets list is a list of strings
+tweets = ['Bright spot from the Panthers\xe2\x80\x99 27-21 loss yesterday? Christian McCaffrey became the fastest player in NFL history to hit 3,000 rushing yards and 3,000 receiving yards. \n\n@fox46\n\nFull #GoodDayCharlotte segment -&gt; https://t.co/2ti9VyJSiH https://t.co/iJpuqox99C',"@EASPORTS_MUT @RobGronkowski @JimmySmithJags @JasonTaylor IF JUSTIN JEFFERSON + DALVIN COOK DON'T GET A CARD SOON... I'll cry",'@new_dash_snow @rainIoss Derrick Henry is the bird the Titans are the person','@JayRBP I\xe2\x80\x99ll never understand Sean Payton using him running when he has Alvin Kamara','\xf0\x9f\x91\x89 $124.99 \xf0\x9f\x91\x88\nNFL PRO LINE Mens Ezekiel Elliott Navy Dallas Cowboys Big Tall Player @topfanscorner \n #PRO #LINE #Mens #Ezekiel #Elliott #Navy #Dallas #NFL #Football #NFLfans \n\nHurry up to get it for the best price!\nhttps://t.co/jgqpxZOYaA', 'Python is 👍']
+
+stemmer = PorterStemmer()
+stopWordsList = set(stopwords.words('english'))
+removeStopWords = ["hasn't", "not", "nor", "couldn't", "haven't", "isn't","mightn't", "aren't", "no", "didn't", "needn't","hadn't","shouldn't", "wouldn't", "doesn't", "mustn't"]
+stopWordsList.difference_update(removeStopWords)
+
+ctr = Counter()
+mostCommonWords = set()
+for i in tweets:
+    for j in i.split(" "):
+        ctr[j] += 1    
+for (i,c) in ctr.most_common(10):       #change 10 to get n most frequent
+    mostCommonWords.add(i.lower())
+
+leastCommonWords = set()
+for (i,c) in ctr.most_common()[:-11:-1]: #change 11 to get n least frequent
+    leastCommonWords.add(i.lower())
+
+def emojiToText(tweet):
+    return emoji.demojize(tweet, delimiters=("", ""))
+
+def removeMostandLeastFrequentWords(tweet):
+    ans = ""
+    for i in tweet.split():
+        if i not in mostCommonWords and i not in leastCommonWords:
+            ans = ans + i + " " 
+    return ans
+
+def stemWords(tweet):
+    ans = ""
+    for i in tweet.split():
+        ans = ans + stemmer.stem(i) + " "
+    return ans
+
+def lemmatizeTweets(tweet):
+    #Lemmatizer
+    wnl = nltk.stem.WordNetLemmatizer()
+    tt = TweetTokenizer()
+    ans = ""
+    for t in tt.tokenize(tweet):
+        ans = ans + wnl.lemmatize(t) + " "
+    return ans
+
+def removeStopWords(tweet):
+    ans = ""
+    tweetWords = tweet.split(" ")
+    for i in tweetWords:
+        if i not in stopWordsList:
+            ans = ans + i + " "
+    return ans
+
+def preprocess(tweets):
+    #tweet processor cleaner
+    ans = []
+    for i in tweets:
+        # convert emojis to text/words
+        afterEmojis = emojiToText(i)  
+        # delete urls and mentions (@)
+        cleaned = p.clean(afterEmojis)
+        # remove digits 
+        removedDigits = re.sub(r'[0-9]', '', cleaned)
+        # remove punctuation
+        removedPunct = re.sub(r'[^\w\s]', '', removedDigits)
+        # make all the text lowercase
+        lowercase = removedPunct.lower()     
+        # remove extra spaces 
+        extraspaces = re.sub(r' +', ' ', lowercase)
+        # apply remove stop words
+        afterStopWords = removeStopWords(extraspaces)
+        # remove most and least frequent words
+        afterFreq = removeMostandLeastFrequentWords(afterStopWords)
+        # stemming
+        afterStemming = stemWords(afterFreq)  #****
+        # apply lemmatizer
+        afterLemmatizer = lemmatizeTweets(afterStemming)
+        #remove trailing/leading spaces
+        extraspaces = afterLemmatizer.strip()
+        ans.append(extraspaces)
+    return ans
+
+def main():
+    res = preprocess(tweets)
+    for i in res:
+        print(i)
+    
+main()
