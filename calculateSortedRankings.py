595fe49282397127c18f36f5ffc786285bb5c639
palchurl <66439919+palchurl@users.noreply.github.com>
2021-12-20 22:30:28 -0500

    Update and rename calculations.py to calculateSortedRankings.py

--- calculateSortedRankings.py
+++ calculateSortedRankings.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import kendalltau
+
 counter = 1 #used to name files
 
 #@param dictionary of sentiment output for top athletes
@@ -102,3 +104,23 @@ player_dic(players11) #create graphs
 player_dic(players12)
 player_dic(players13)
 player_dic(players14)
+
+###### Evaluation Process:
+# Step 1: In sentiment_output, sort players by some measure of positive sentiment
+# Step 2: In week11.txt, assign each player a number 1-20
+# Step 3: In sentiment_output, assign each player numbers according to numbers from week11.txt, new players not on week11.txt will be given numbers >20
+# Step 4: Create two lists with those numbers, one list will represent the actual rankings 1-20 from week11.txt, the other will represent the players ranked by sentiment_output
+# Step 5: Calculate kendalltau between both lists to determine the correlation
+
+# Interpretation of Kendall Tau
+# Notice that the maximum and minimum value that 𝛕 can take is +1 and -1 respectively. +1 denotes perfect agreement and -1 denotes complete disagreement. 
+# A value of 0 means that there is no correlation between the rankings. 
+
+# Taking values from the above example in Lists
+#manually replaced these two lists and uncommented the following lines to get the kendall rank correlation score
+#rankings_sentiment = [1, 3, 11, 2, 7, 4, 5, 9, 10, 6, 12, 13, 14, 21, 16, 17, 8, 19, 20, 22]
+#rankings_actual = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] 
+  
+# Calculating Kendall Rank correlation
+#corr, _ = kendalltau(rankings_actual, rankings_sentiment)
+#print('Kendall Rank correlation: %.5f' % corr)
